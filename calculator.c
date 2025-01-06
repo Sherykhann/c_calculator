21258eff238d8e9aa5b7ae989d90e938f0f5fec5
Shehryar Khan <44238005+Sherykhann@users.noreply.github.com>
2025-01-05 22:29:59 -0500

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -10,26 +10,27 @@ int main(void)
 {
     float first_number, second_number;
     char operator_symbol;
+    /*Enter the first number*/ 
     printf("Enter first number: ");
     scanf("%f", &first_number);
-
+   
     printf("Enter operator: ");
     scanf(" %c", &operator_symbol);
-
+     /*Enter the second number*/ 
     printf("Enter second number: ");
     scanf("%f", &second_number);
 
-    if (operator_symbol == '+')
+    if (operator_symbol == '+')  /*Perform the sum operation*/ 
     {
         printf("Sum is %0.2f\n", (first_number + second_number));
     }
 
-    if (operator_symbol == '-')
+    if (operator_symbol == '-') /*Perform the difference operation*/ 
     {
         printf("Difference is %0.2f\n", (first_number - second_number));
     }
 
-    if (operator_symbol == '*')
+    if (operator_symbol == '*') /*Perform the product operation*/ 
     {
         printf("Product is %0.2f\n", (first_number * second_number));
     }
